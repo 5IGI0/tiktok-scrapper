875db8b912232ecf17dc21c909a35ffffa3d0e81
5IGI0 <5IGI0@protonmail.com>
2022-07-27 09:01:07 +0200

    encore plus rapide

--- selenium_controler.py
+++ selenium_controler.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep, time
-from json import loads, load, dumps
+from json import loads, load, dumps, dump
 from table_manager import is_present, add_presence
 import requests
 import tarfile
@@ -18,9 +18,9 @@ with open("cookies.json") as fp:
 	for item in load(fp):
 		driver.add_cookie(item)
 driver.get("https://www.tiktok.com/following")
+driver.maximize_window()
 
 driver.execute_script("window.open('about:blank','_blank');")
-sleep(2)
 driver.switch_to.window(driver.window_handles[0])
 
 # on attend que le gars se connecte et va dans ses abonnements
@@ -30,6 +30,13 @@ driver.switch_to.window(driver.window_handles[0])
 #	sleep(5)
 
 olds = []
+user_cache = {}
+
+try:
+	with open("user_cache.json", "r") as fp:
+		user_cache = load(fp)
+except:
+	pass
 
 DOWNLOAD_DIR="./data/content/"
 
@@ -65,35 +72,46 @@ def download(url, data):
 	add_presence(data["video_id"], data["author_id"])
 	print("done.")
 
+thread_container = driver.find_element_by_xpath("/html")
+i = 0
 while True:
-	elements = driver.find_elements_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div")
-	thread_container = driver.find_element_by_xpath("/html")
 	action = webdriver.ActionChains(driver)
+	elements = driver.find_elements_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div")
+	i += 1
 	for elem in elements:
 		if elem.id not in olds:
 			try:
 				author_link = elem.find_element_by_xpath("./a").get_attribute("href")
 				author = author_link.split("/@")[1].split("?")[0]
 				video_url = elem.find_element_by_xpath("./div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/video").get_attribute("src")
+
+				#####
+				# je récupère l'invite what's app pour avoir l'id
+				#####
+				share = elem.find_element_by_xpath("./div[1]/div[2]/div[2]/button[3]")
+				action.move_to_element(share)
+				action.perform()
+				sleep(0.2)
+				video_id = share.find_element_by_xpath("./div[1]/div[1]/a[3]").get_attribute("href").split("%2Fvideo%2F")[1].split("%3F")[0]
 			except:
 				continue
 
-			#####
-			# je récupère l'invite what's app pour avoir l'id
-			#####
-			share = elem.find_element_by_xpath("./div[1]/div[2]/div[2]/button[3]")
-			action.move_to_element(share)
-			action.perform()
-			sleep(0.2)
-			video_id = share.find_element_by_xpath("./div[1]/div[1]/a[3]").get_attribute("href").split("%2Fvideo%2F")[1].split("%3F")[0]
-
+			thread_container.send_keys(Keys.ARROW_DOWN)
 			if not is_present(video_id):
 				# on vas dans l'onglet qu'on vient d'ouvrir
-				driver.switch_to.window(driver.window_handles[1])
-				driver.get(author_link)
-				author_data = loads(driver.find_element_by_id("SIGI_STATE").get_attribute("innerText"))["UserModule"]["users"][author]
+				if not author in user_cache:
+					print(author, "not cached, getting")
+					driver.switch_to.window(driver.window_handles[1])
+					driver.implicitly_wait(1)
+					driver.get(author_link)
+					user_cache[author] = loads(driver.find_element_by_id("SIGI_STATE").get_attribute("innerText"))["UserModule"]["users"][author]
+					driver.switch_to.window(driver.window_handles[0])
+					with open("user_cache.json", "w") as fp:
+						dump(user_cache, fp)
+				else:
+					print(author, "cached!")
+				author_data = user_cache[author]
 				author_id = author_data["id"]
-				driver.switch_to.window(driver.window_handles[0])
 				
 				print("author id:", author_id)
 				print("video url:", video_url)
@@ -107,7 +125,8 @@ while True:
 			else:
 				print("already downloaded, skip")
 
-			thread_container.send_keys(Keys.ARROW_DOWN)
-			olds += elements[:elements.index(elem)+1]
-			break
-	sleep(0.75)
+			i = 0
+			olds.append(elem.id)
+			sleep(0.1)
+	if i >= 5:
+		i = 0
